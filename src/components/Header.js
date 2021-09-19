a37ae974907c725e0034b1e26f1ae4c63f8305f0
chantaldesiree <chantaldesiree@outlook.com>
2021-09-19 15:26:21 -0500

    Created Header with drawer.

---
+++ src/components/Header.js
@@ -0,0 +1,160 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+
+function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+
+  return (
+    <Container>
+      <a href="#">
+        <img src="/images/logo.svg" alt="" />
+      </a>
+      <Menu>
+        <a href="#">Model S</a>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model Y</a>
+      </Menu>
+      <RightMenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla Account</a>
+      </RightMenu>
+      <CustomMenu onClick={() => setBurgerStatus(true)} />
+      <BurgerNav show={burgerStatus}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setBurgerStatus(false)} />
+        </CloseWrapper>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-In</a>
+        </li>
+        <li>
+          <a href="#">Test Drive</a>
+        </li>
+        <li>
+          <a href="#">Cybertruck</a>
+        </li>
+        <li>
+          <a href="#">Roadster</a>
+        </li>
+        <li>
+          <a href="#">Semi</a>
+        </li>
+        <li>
+          <a href="#">Charging</a>
+        </li>
+        <li>
+          <a href="#">Powerwall</a>
+        </li>
+        <li>
+          <a href="#">Commercial Energy</a>
+        </li>
+
+        <li>
+          <a href="#">Utilities</a>
+        </li>
+
+        <li>
+          <a href="#">Find Us</a>
+        </li>
+
+        <li>
+          <a href="#">Support</a>
+        </li>
+        <li>
+          <a href="#">Investor Relations</a>
+        </li>
+      </BurgerNav>
+    </Container>
+  );
+}
+
+export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: no-wrap;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`;
+
+const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
+
+  a {
+    font-weight: 600;
+    text-transform: uppercase;
+    padding: 0 10px;
+    flex-wrap: no-wrap;
+  }
+`;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  font-weight: 600;
+  font-size: 15px;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 10;
+  list-style: none;
+  padding: 40px 0px 40px 40px;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+
+  li {
+    margin: 15px 0px;
+    padding: 7px 7px;
+    border-radius: 10px 0px 0px 10px;
+  }
+
+  li:hover {
+    background: #eeeeee;
+  }
+`;
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 40px;
+`;
