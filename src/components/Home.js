cf430a9810cbab9c3d659a79a7efb813c2a5f56c
chantaldesiree <chantaldesiree@outlook.com>
2021-11-08 22:37:31 -0600

    Removed unneeded import.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,15 @@ import React from "react";
 import styled from "styled-components";
 
 import Section from "./Section";
-import Footer from "./Footer";
 
 /**
-  * Home component is a container for all of the sections
-  */
+ * Home component is a container for all of the sections
+ */
 function Home() {
-
   /**
-  * sectionInfo is an array of objects that contains all of the information
-  * for every section, including titles, descriptions, button text and background images.
-  */
+   * sectionInfo is an array of objects that contains all of the information
+   * for every section, including titles, descriptions, button text and background images.
+   */
   const sectionInfo = [
     {
       title: "Model S",
@@ -20,51 +18,56 @@ function Home() {
       leftBtnText: "Custom Order",
       rightBtnText: "Existing Inventory",
       backgroundImage: "model-s.jpg",
-    },{
+    },
+    {
       title: "Model 3",
       description: "Order Online for Touchless Delivery",
       leftBtnText: "Custom Order",
       rightBtnText: "Existing Inventory",
       backgroundImage: "model-3.jpg",
-    },{
+    },
+    {
       title: "Model X",
       description: "Order Online for Touchless Delivery",
       leftBtnText: "Custom Order",
       rightBtnText: "Existing Inventory",
       backgroundImage: "model-x.jpg",
-    },{
+    },
+    {
       title: "Model Y",
       description: "Order Online for Touchless Delivery",
       leftBtnText: "Custom Order",
       rightBtnText: "Existing Inventory",
       backgroundImage: "model-y.jpg",
-    },{
+    },
+    {
       title: "Solar and Powerwall",
       description: "Power Everything",
       leftBtnText: "Learn More",
       rightBtnText: null,
       backgroundImage: "solar-panel.jpg",
-    },{
+    },
+    {
       title: "Accessories",
       description: "",
       leftBtnText: "Shop Now",
       rightBtnText: null,
       backgroundImage: "accessories.jpg",
     },
-  ]
+  ];
   return (
     <Container>
-
-    {sectionInfo.map((section) => {
-      <Section
-      title={section.title}
-      description={section.description}
-      leftBtnText={section.leftBtnText}
-      rightBtnText={section.rightBtnText}
-      backgroundImage={section.backgroundImage}
-    />
-    })}
-      
+      {sectionInfo.map((section) => {
+        return (
+          <Section
+            title={section.title}
+            description={section.description}
+            leftBtnText={section.leftBtnText}
+            rightBtnText={section.rightBtnText}
+            backgroundImage={section.backgroundImage}
+          />
+        );
+      })}
     </Container>
   );
 }
